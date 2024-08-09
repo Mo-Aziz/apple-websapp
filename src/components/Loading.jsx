3f82c310a9e2c18b3dc34023f5c7ae614d26b4fa
Mo-Aziz <mohamed.aref32@gmail.com>
2024-08-09 11:14:20 -0500

    implemneted iphone 3d model with threejs

---
+++ src/components/Loading.jsx
@@ -0,0 +1,86 @@
+// import { Html } from "@react-three/drei";
+// import React from "react";
+
+// import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+
+// const Loader = () => {
+//   const percentage = 66;
+//   return (
+//     <Html>
+//       <div className="w-full h-full flex justify-center items-center top-0 left-0">
+//         <div className="w-[10w] h-[10vw] rounded-full">
+//           <CircularProgressbar
+//             value={percentage}
+//             text={`${percentage}%`}
+//             styles={buildStyles({
+//               // Rotation of path and trail, in number of turns (0-1)
+//               rotation: 0.25,
+
+//               // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+//               strokeLinecap: "butt",
+
+//               // Text size
+//               textSize: "25px",
+
+//               // How long animation takes to go from one percentage to another, in seconds
+//               pathTransitionDuration: 0.5,
+
+//               // Can specify path transition in more detail, or remove it entirely
+//               // pathTransition: 'none',
+
+//               // Colors
+//               pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
+//               textColor: "#f88",
+//               trailColor: "#d6d6d6",
+//               backgroundColor: "#3e98c7",
+//             })}
+//           />
+//           ;
+//         </div>
+//       </div>
+//     </Html>
+//   );
+// };
+
+// export default Loader;
+
+// const percentage = 66;
+
+// <CircularProgressbar
+//   value={percentage}
+//   text={`${percentage}%`}
+//   styles={buildStyles({
+//     // Rotation of path and trail, in number of turns (0-1)
+//     rotation: 0.25,
+
+//     // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+//     strokeLinecap: "butt",
+
+//     // Text size
+//     textSize: "16px",
+
+//     // How long animation takes to go from one percentage to another, in seconds
+//     pathTransitionDuration: 0.5,
+
+//     // Can specify path transition in more detail, or remove it entirely
+//     // pathTransition: 'none',
+
+//     // Colors
+//     pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
+//     textColor: "#f88",
+//     trailColor: "#d6d6d6",
+//     backgroundColor: "#3e98c7",
+//   })}
+// />;
+
+// import React from "react";
+
+// const Loading = () => {
+//   return (
+//     <div className="spinner-container">
+//       <div className="spinner"></div>
+//     </div>
+//   );
+// };
+
+// export default Loading;
