24330f1bdbefe601b20f7020d45dc137471b492e
Mo-Aziz <mohamed.aref32@gmail.com>
2024-08-09 23:03:55 -0500

    finishied the features section with its animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import Navbar from "./components/Navbar";
 
+import * as Sentry from "@sentry/react";
 const App = () => {
   return (
     <main className="bg-black">
@@ -10,8 +12,9 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
     </main>
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
